ee8d8a150957a5e266d28ffcbde604d53d8a162d
MartinJeanne <martinjeanne.dev@gmail.com>
2022-07-28 15:17:40 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: { HelloWorld }
+}
+</script>
+
+<template>
+  <header>
+    <h1>Welcome on my portfolio</h1>
+  </header>
+</template>
+
+<style scoped>
+</style>
