fcd1eebc148dd3110fa8cebf7d874ec0c3e28773
MartinJeanne <martinjeanne.dev@gmail.com>
2022-07-28 16:11:12 +0200

    Grid implemented

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,46 @@ export default {
 </script>
 
 <template>
-  <header>
-    <h1>Welcome on my portfolio</h1>
-  </header>
+
+  <article>
+    <div class="cell small red">
+      <h1>Welcome on my portfolio</h1>
+    </div>
+
+    <div class="cell big black">
+      <img src="public/vite.svg" alt="Vite logo" width="50%" style="">
+      <img src="src/assets/vue.svg" alt="Vite logo" width="50%" style="">
+    </div>
+  </article>
+
+  <article>
+    <div class="cell big white">
+      <p>There will be good stuff in here !</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil suscipit
+        beatae quia voluptatem tenetur temporibus quibusdam doloribus !
+      </p>
+    </div>
+
+    <div class="cell small red">
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  </article>
+
+  <article>
+    <div class="cell small red">
+      <p>There will be good stuff in here !</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil suscipit
+        beatae quia voluptatem tenetur temporibus quibusdam doloribus !
+      </p>
+    </div>
+
+    <div class="cell big black">
+      <p>Lorem, ipsum dolor.</p>
+    </div>
+  </article>
+
 </template>
 
 <style scoped>
