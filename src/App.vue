a715711614a72eb5e40b4f7664d3aaf31d8c47fc
MartinJeanne <martinjeanne.dev@gmail.com>
2022-07-29 10:15:26 +0200

    Re changing grid (to simplify it) now using one only grid + img style&size is fixed !

--- src/App.vue
+++ src/App.vue
@@ -7,45 +7,45 @@ export default {
 </script>
 
 <template>
-
-  <header class="grid last-big">
-    <div class="cell red">
-      <h1>Welcome on my portfolio</h1>
-    </div>
-
-    <div class="cell black">
-      <img src="src/assets/pp_cool.jpg" alt="Photo de profil">
-    </div>
+  <header class="short red">
+    <h1>Welcome on my portfolio</h1>
   </header>
 
-  <article class="grid first-big">
-    <div class="cell white">
-      <p>There will be good stuff in here !</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil suscipit
-        beatae quia voluptatem tenetur temporibus quibusdam doloribus !
-      </p>
-    </div>
-
-    <div class="cell red">
-      <p>Lorem ipsum dolor sit amet.</p>
-    </div>
+  <article class="tall white">
+    <p>There will be good stuff in here !</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil suscipit
+      beatae quia voluptatem tenetur temporibus quibusdam doloribus !
+    </p>
   </article>
 
-  <footer class="grid last-big">
-    <div class="cell red">
-      <p>There will be good stuff in here !</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil suscipit
-        beatae quia voluptatem tenetur temporibus quibusdam doloribus !
-      </p>
-    </div>
+  <article class="short red">
+    <p>There will be good stuff in here !</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil suscipit
+      beatae quia voluptatem tenetur temporibus quibusdam doloribus !
+    </p>
+  </article>
 
-    <div class="cell black">
-      <p>Lorem, ipsum dolor.</p>
-    </div>
+  <div class="tall black">
+    <img src="src/assets/pp_cool.jpg" alt="Photo Martin" width="1196" height="2301"/>
+  </div>
+
+  <footer class="tall black">
+    <p>There will be good stuff in here !</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil suscipit
+      beatae quia voluptatem tenetur temporibus quibusdam doloribus !
+    </p>
   </footer>
 
+  <article class="short red">
+    <p>There will be good stuff in here !</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil suscipit
+      beatae quia voluptatem tenetur temporibus quibusdam doloribus !
+    </p>
+  </article>
 </template>
 
 <style scoped>
